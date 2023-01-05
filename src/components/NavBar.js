f929d55173074ce9d1eab82a00bb17bf76448cf5
Isaac <deisaacalejandro@gmail.com>
2023-01-05 13:55:36 -0400

    added home nav to logo

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -12,12 +12,21 @@ function NavBar() {
         setIsHidden(!isHidden)
     }
 
+    function handleLogoClick(event) {
+        console.log()
+        return (
+            <NavLink to="/"> </NavLink>
+        )
+    }
+
+
+
     const hamStackClasses = `z-40 block hamburger mr-4 md:hidden focus:outline-none ${isOpen ? 'open' : ''}`
     const menuClasses = `absolute top-0  bottom-0 left-0 ${isHidden ? 'hidden' : ''} flex-col self-end w-full min-h-screen py-1 pt-40 pl-12 space-y-2 text-lg text-white uppercase bg-black`
 
     return (
         <nav className="flex items-center justify-between font-bold text-white bg-black">
-            <img className="h-32 p-6 hover:object-scale-down hover:scale-105 duration-200" src={logo} alt="Logo" />
+            <NavLink to="/"><img className="h-32 p-6 hover:object-scale-down hover:scale-105 duration-200" onClick={handleLogoClick} src={logo} alt="Logo" /> </NavLink>
             <div className="hidden h-10 font-alata md:flex md:space-x-8 pr-9">
 
                 <div className="group">
